be91b55749968085bdb43fcbc874931632317755
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2019-04-04 16:05:49 +0700

    [ADD] add inputs folder in view and add 1st input template

---
+++ resources/views/inputs/input.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Document</title>
+  <style>
+    .input-group-text {
+      padding: 0.1rem 0.1rem;
+      background-color: #405377;
+    }
+
+    .input-group {
+      width: 33.072916666vw;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="input-group mb-3 mr-auto ml-auto">
+    <div class="input-group-prepend">
+      <span class="input-group-text"></span>
+    </div>
+    <input type="text" class="form-control">
+</body>
+
+</html>
