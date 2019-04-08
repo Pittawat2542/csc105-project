1768239274e8637871ac68922119b18be14f3786
Michał Zieliński <zielu0@gmail.com>
2019-04-08 18:07:28 +0700

    fixed bootstrap source in theme and added views files for edit and index users

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -5,7 +5,7 @@
 <script src="{{asset('js/jquery.min.js')}}"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="{{asset('js/bootstrap.min.js')}}'"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
 
 <!-- Metis Menu Plugin JavaScript -->
 <script src="{{asset('js/metisMenu.min.js')}}"></script>
