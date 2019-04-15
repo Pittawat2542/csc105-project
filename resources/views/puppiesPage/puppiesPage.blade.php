0d7053ea052a49b0dc48a1bcc24102082026a75d
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2019-04-15 15:04:56 +0700

    [update] rename html file and complete mock-up

--- resources/views/puppiesPage/puppiesPage.blade.php
+++ resources/views/puppiesPage/puppiesPage.blade.php
@@ -73,7 +73,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-7">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-6">
                 <div class="d-flex flex-column">
                     <div class="float-right">
                         <img src="../../../design/assets/like.png" class="like float-right" alt="">
@@ -97,13 +98,95 @@
                         <h5 class="float-right basic-info">Basic info: some basic info</h5>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col p-0">
                             <hr>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-5 text-center" id="line">
+                            <a href="https://line.me/th/" target="blank">
+                                <h4 class="mt-2"><img id="line-img" src="../../../design/assets/line.png" alt=""> Line
+                                </h4>
+                            </a>
+                        </div>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-5 text-center " id="fb">
+                            <a href="https://www.facebook.com/" target="blank">
+                                <h4 class="m-3"><img id="fb-img" src="../../../design/assets/facebook-logo.png"
+                                        alt="">Facebook</h4>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-5 text-center p-2" id="mail">
+                            <a href="mailto:someone@example.com" target="blank">
+                                <h4 class="mt-2"><img id="mail-img" src="../../../design/assets/close-envelope.png"
+                                        alt=""> Mail</h4>
+                            </a>
+                        </div>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-5 text-center " id="call">
+                            <a href="tel:+66123456789" target="blank">
+                                <h4 class="m-3"><img id="call-img" src="../../../design/assets/phone-receiver.png"
+                                        alt="">Call</h4>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <br>
+        <br>
+        <hr>
+        <h1 id="description" class="mt-5">DESCRIPTION</h1>
+        <h3 class="display-5" id="info">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum laoreet nisi nec condimentum.
+                In
+                pellentesque sem et sapien egestas sodales. Donec vel tempus eros, eget aliquam ipsum. Quisque commodo,
+                ex
+                in placerat placerat, massa neque fermentum enim, fringilla mollis nibh justo a mauris. Integer nec
+                lacus
+                augue. Donec porta urna est. Phasellus malesuada justo quis turpis venenatis, euismod aliquet tellus
+                tristique. Sed in leo dui. Praesent volutpat eget dolor eu gravida. Praesent eu rhoncus nibh. Vivamus
+                tristique est dui, nec sagittis neque placerat in. Quisque arcu ex, laoreet sit amet libero sed,
+                faucibus
+                mollis velit. Morbi eu ligula porta, tincidunt purus non, dapibus odio.</p>
+            <p>In hac habitasse platea
+                dictumst.
+                Phasellus tempor consequat orci iaculis imperdiet. Morbi elementum leo at urna aliquet malesuada. Duis
+                varius dui tortor, eget auctor ex viverra ac. Ut dictum accumsan dictum. In eu sem suscipit tellus
+                blandit
+                imperdiet ut in odio. Cras vel magna euismod, dignissim magna at, rhoncus mi. Morbi nisi purus,
+                efficitur
+                sit amet nunc eget, suscipit volutpat urna. Nunc mollis luctus risus, ac consequat nulla consequat sed.
+                Maecenas in velit quis elit eleifend fringilla vitae eu lectus. Suspendisse lectus est, facilisis eu
+                ultrices et, ullamcorper eu mauris. Aliquam suscipit imperdiet neque, in mollis quam rutrum eget.</p>
+            <p>Sed
+                pellentesque ante a justo placerat auctor. Phasellus consectetur turpis in accumsan vulputate. Curabitur
+                elementum felis est, vitae fringilla est luctus at. Phasellus vitae libero diam. Nunc eleifend augue non
+                dolor feugiat suscipit. Maecenas consequat ipsum lacinia, fermentum mauris nec, pharetra lacus. Aliquam
+                ut
+                sagittis enim, consequat consectetur risus. Sed augue elit, maximus nec placerat ac, sagittis ac magna.
+                Praesent semper nulla eu faucibus porta.</p>
+            <p> Suspendisse potenti. Vivamus vel dignissim leo. Curabitur a aliquet
+                tortor. Praesent ac varius erat. In at venenatis ex. Maecenas sit amet facilisis nunc. Proin ullamcorper
+                et
+                quam vel ultrices. Sed euismod arcu eleifend, bibendum lorem vel, placerat lacus. Quisque leo risus,
+                cursus
+                eu mollis id, pharetra sit amet sapien. Nulla facilisi. Sed rutrum porttitor lorem, quis pharetra felis
+                lacinia nec.</p>
+            <p> Mauris ut sem vel massa placerat rhoncus vitae at neque. Cras dapibus, sapien id ultricies
+                lobortis, quam justo fermentum nisl, eget euismod est nisi quis est. Nulla id facilisis dui. In
+                elementum
+                orci laoreet diam facilisis, vel tempus nulla convallis. In eu ultrices enim. Aliquam sed nunc a mi
+                luctus
+                ullamcorper. Vivamus hendrerit urna sit amet metus euismod tristique. Praesent quis lectus ut dui mollis
+                aliquet sit amet ac nisi. Phasellus finibus congue dapibus.</p>
+        </h3>
+    </div>
+    <br>
+    <div class="jumbotron bg-black no-rounding mt-5">
+        <h1 class="display-5 font-weight-bold text-center">FOOTER</h1>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
