72a91c75fda913062762d7eaf68b75df2042d262
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-04-19 13:05:35 +0700

    [ADD] navbar

---
+++ resources/views/navbar/navbar.blade.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+    <style>
+        nav {
+            -webkit-box-shadow: 0px 5px 5px 0px rgba(0,0,0,0.3);
+            -moz-box-shadow: 0px 5px 5px 0px rgba(0,0,0,0.3);
+            box-shadow: 0px 5px 5px 0px rgba(0,0,0,0.3);
+        }
+
+        .navbar-nav .nav-link.nav-title {
+            color: #405377;
+            font-size: 1.25rem;
+            font-weight: 600;
+        }
+
+        .btn-link.nav-link-main {
+            color: #405377;
+            font-weight: 700;
+            text-transform: uppercase;
+        }
+
+        .search-icon {
+            color: #405377;
+            font-size: 1.25rem;
+        }
+
+        .search-box {
+            background-color: inherit;
+            border: none;
+            border-bottom: 1px solid #405377;
+            border-radius: 0;
+        }
+
+        .search-box::placeholder {
+            color: #405377;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/"><img src="https://via.placeholder.com/50" alt=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link nav-title" href="#">Luk Mha Shop</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <label class="mr-2 search-icon" for=""><i class="fas fa-search"></i></label>
+            <input class="form-control mr-sm-2 search-box" type="search" placeholder="Search . . ." aria-label="Search">
+        </form>
+        <button type="button" class="btn btn-link nav-link-main">Register</button>
+        <button type="button" class="btn btn-link nav-link-main">Login</button>
+    </div>
+</nav>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
