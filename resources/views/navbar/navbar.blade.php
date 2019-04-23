8e0e9cd5b55d5a17b745b867f99720894ab10178
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-04-23 15:09:48 +0700

    [UPDATE] navbar

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -1,20 +1,17 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/"><img src="https://via.placeholder.com/50" alt=""></a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light box-shadow">
+    <a class="navbar-brand" href="/"><img class="img-responsive mr-3" src="/images/logo.png" alt="logo">Puppies Hub</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link nav-title" href="/">Luk Mha Shop</a>
-            </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
             <label class="mr-2 search-icon" for=""><i class="fas fa-search"></i></label>
-            <input class="form-control mr-sm-2 search-box" type="search" placeholder="Search . . ." aria-label="Search">
+            <input class="form-control mr-sm-2 search-box box-shadow" type="search" placeholder="Search . . ." aria-label="Search">
         </form>
-        <a href="/static/register" class="btn btn-link nav-link-main" role="button">Register</a>
-        <a href="/static/login" class="btn btn-link nav-link-main" role="button">Login</a>
+        <a href="/static/register" class="btn btn-link nav-link-main text-uppercase" role="button">Register</a>
+        <a href="/static/login" class="btn btn-link nav-link-main text-uppercase" role="button">Login</a>
     </div>
 </nav>
