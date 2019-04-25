46cc3d5da3174ead6b94e69df195c37c171923c6
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-04-25 12:38:31 +0700

    [UPDATE] responsive support for auth page

--- resources/views/static/auth/login.blade.php
+++ resources/views/static/auth/login.blade.php
@@ -38,12 +38,12 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-6 col-sm-12">
-                                <div class="form-button">
+                                <div id="btn-register" class="form-button">
                                     <button type="submit" class="btn btn-outline-primary">Register</button>
                                 </div>
                             </div>
                             <div class="col-md-6 col-sm-12">
-                                <div class="form-button">
+                                <div id="btn-login" class="form-button">
                                     <button type="submit" class="btn btn-primary">Login</button>
                                 </div>
                             </div>
