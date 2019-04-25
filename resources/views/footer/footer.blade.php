6b920ae98a9a7d5913e11c908f346663194a8347
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-04-25 12:29:11 +0700

    [UPDATE] responsive for footer

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="container pt-5 pb-3">
         <div class="row">
-            <div class="col"><h4>About Puppies Hub</h4>
+            <div class="col-md-6 col-sm-12"><h4>About Puppies Hub</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt illum sint mollitia quos
                     obcaecati, accusantium dolores ratione beatae magnam laborum officia dolore facere, consequatur
                     tenetur sapiente voluptatem tempora architecto fugiat!</p></div>
-            <div class="col"><h4>Contact Us</h4>
+            <div class="col-md-6 col-sm-12"><h4>Contact Us</h4>
                 <p><span class="font-weight-bold">Tel:</span> 012-3456-789</p>
                 <p><span class="font-weight-bold">Email:</span> info@puppieshub.com</p>
                 <p><span class="font-weight-bold">Address:</span> 126 Pracha Uthit Rd., Bang Mod, Thung Khru, Bangkok 10140, Thailan</p>
