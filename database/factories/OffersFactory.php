e74c652be3d8f1f6042265a0573e1620ce9de37e
Michał Zieliński <zielu0@gmail.com>
2019-05-08 09:54:30 +0700

    [FIX] Storage f google cloud

--- database/factories/OffersFactory.php
+++ database/factories/OffersFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\offers::class, function (Faker $faker) {
     return [
-        //
+        'title'=>$faker->title,
+        'name'=>$faker->firstName
     ];
 });
--- database/factories/PhotoFactory.php
+++ database/factories/PhotoFactory.php
@@ -1,9 +1,13 @@
 <?php
-
+use App\Photo;
+use App\User;
 use Faker\Generator as Faker;
 
-$factory->define(App\Photo::class, function (Faker $faker) {
+
+$factory->define(Photo::class, function (Faker $faker) {
     return [
-        //
-    ];
+        'path'=>$faker->image($dir = '/uploads/images'),
+        'user_id'=>$factory->create(User::class)->id,
+        'offer_id'=>3
+        ];
 });
