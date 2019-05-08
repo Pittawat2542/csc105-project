1c9647cb073b6d8a761badf1b7ef0d8a973847f3
Michał Zieliński <zielu0@gmail.com>
2019-05-08 15:31:31 +0700

    small fixes

--- public/js/favorite.js
+++ public/js/favorite.js
@@ -1,13 +1,13 @@
 function clickFav(x){
     if(x.classList.contains("like")){
-        // $puppy_id = $(this).val();
-        // $.ajax({
-        //     url: '/wishlist/' + brand_id + '/store',
-        //     data: {
-        //         _method: 'GET',
-        //     },
-        //     type: "POST",
-        // });
+        var puppy_id = $(this).val();
+        $.ajax({
+            url: '/wishlist/' + puppy_id + '/store',
+            data: {
+                _method: 'GET',
+            },
+            type: "POST",
+        });
         x.classList.add("liked");
         x.classList.remove("like");
     }else{
